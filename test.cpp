f2fb9bcf55780d7ea413ebdd87a11a3916f581d2
Livio123123 <99071165+Livio123123@users.noreply.github.com>
2022-06-30 13:39:22 +0700

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //print text
+    cout<<"hello world";
+    // deklarasi variabel
+    string b;
+    //ambil inputan user dan di simpan di variabel "a"
+    cin>>b;
+    cout<<b;
+    return 0;
+}
+
+
+
+
